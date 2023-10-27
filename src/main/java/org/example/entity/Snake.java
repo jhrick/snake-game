633670f3c0709a67329e01374011eeecfaa63887
jhrick <hj.rickcp@gmail.com>
2023-10-27 20:59:16 -0300

    feat: add grow function

--- src/main/java/org/example/entity/Snake.java
+++ src/main/java/org/example/entity/Snake.java
@@ -4,7 +4,7 @@ import org.example.GamePanel;
 import org.example.KeyHandler;
 
 import java.awt.*;
-import java.util.Objects;
+import java.util.LinkedList;
 
 public class Snake extends Entity {
 
@@ -13,11 +13,16 @@ public class Snake extends Entity {
 
     boolean running;
 
+    LinkedList<Piece> pieces = new LinkedList<>();
+
     public Snake(KeyHandler keyH, GamePanel gp) {
         this.keyH = keyH;
         this.gp = gp;
 
         setDefaultValues();
+
+        pieces.addFirst(new Piece(this.x, this.y));
+        pieces.add(new Piece((pieces.get(0).getX() - gp.tileSize), (pieces.get(0).getY() - gp.tileSize)));
     }
 
     public void setDefaultValues() {
@@ -31,28 +36,60 @@ public class Snake extends Entity {
     }
 
     private void move() {
-        if (Objects.equals(keyH.direction, "left")) {
-            x -= gp.tileSize;
-        } else if (Objects.equals(keyH.direction, "right")) {
-            x += gp.tileSize;
-        } else if (Objects.equals(keyH.direction, "up")) {
-            y -= gp.tileSize;
-        } else if (Objects.equals(keyH.direction, "down")) {
-            y += gp.tileSize;
+        String direction = keyH.direction;
+
+        for (int i = pieces.size() - 1; i > 0; i--) {
+            pieces.get(i).setX(pieces.get(i - 1).getX());
+            pieces.get(i).setY(pieces.get(i - 1).getY());
+        }
+
+        switch (direction) {
+            case "left": {
+                x -= gp.tileSize;
+                pieces.get(0).setX(x);
+                break;
+            }
+            case "right": {
+                x += gp.tileSize;
+                pieces.get(0).setX(x);
+                break;
+            }
+            case "up": {
+                y -= gp.tileSize;
+                pieces.get(0).setY(y);
+                break;
+            }
+            case "down": {
+                y += gp.tileSize;
+                pieces.get(0).setY(y);
+                break;
+            }
         }
     }
 
-    public boolean pickApple(Apple apple) {
+    public Apple checkApple(Apple apple) {
         int appleXPosition = apple.getX();
         int appleYPosition = apple.getY();
 
-        return this.x == appleXPosition && this.y == appleYPosition;
+        if (this.x == appleXPosition && this.y == appleYPosition) {
+            apple = new Apple(gp);
+
+            pieces.addLast(new Piece(pieces.get(pieces.size() - 1).getX(), pieces.get(pieces.size() - 1).getY()));
+
+            System.out.printf("new apple!!x: %d; y %d", apple.getX(), apple.getY());
+
+            return apple;
+        }
+
+        return apple;
     }
 
     public void render(Graphics2D g2) {
         g2.setColor(Color.green);
 
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        for (Piece piece : pieces) {
+            g2.fillRect(piece.getX(), piece.getY(), gp.tileSize, gp.tileSize);
+        }
     }
 
     public boolean isRunning() {
